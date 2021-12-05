02d2d1844615c40d0589886a041b9245f7f70f3b
Argha Chakrabarty <ac380012@gmail.com>
2021-12-05 18:36:59 +0530

    whiteboard done

--- board.py
+++ board.py
@@ -3,6 +3,7 @@ import os
 import time
 
 import pygame
+import pygame.gfxdraw
 from pygame.locals import *
 
 pygame.init()
@@ -15,8 +16,8 @@ win = pygame.display.set_mode((win_wt, win_ht))
 font = pygame.font.SysFont("Arial", 15)
 
 WHITE = "#ffffff"
-BLACK = "000000"
-PIXEL_SIZE = 10
+BLACK = "#000000"
+PIXEL_SIZE = 15
 
 # ==========================================================================================
 
@@ -67,7 +68,7 @@ def update_fps():
 
 class Cursor(object):
     def __init__(self):
-        self.color = pygame.Color("#000000")
+        self.color = "#000000"
         self.size = 4
 
     def get_grid_pos(self, pos):
@@ -79,7 +80,24 @@ class Cursor(object):
         return row, col
 
     def draw(self):
-        pygame.draw.circle(win, self.color, (pygame.mouse.get_pos()), self.size)
+        pygame.draw.circle(win, pygame.Color(self.color), (pygame.mouse.get_pos()), self.size)
+
+
+class Button(object):
+    def __init__(self, x, y, color):
+        self.color = color
+        self.size = 30
+        self.rect = pygame.Rect(x, y, self.size, self.size)
+
+    def draw(self):
+        pygame.draw.rect(win, pygame.Color(self.color), [self.rect.x, self.rect.y, self.size, self.size])
+        pygame.draw.rect(win, pygame.Color(BLACK), [self.rect.x, self.rect.y, self.size, self.size], width=3)
+
+    def change_color(self, cursor):
+        if self.rect.collidepoint(*pygame.mouse.get_pos()):
+            cursor.color = self.color
+            pass
+
 
 
 class Board():
@@ -90,14 +108,20 @@ class Board():
 
     def draw_grid(self):
         for i in range(self.y):
-            pygame.draw.line(win, pygame.Color(40, 40, 40), (0, i * PIXEL_SIZE), (win_wt, i * PIXEL_SIZE))
+            # pygame.gfxdraw.line(win, pygame.Color(40, 40, 40), (0, i * PIXEL_SIZE), (win_wt, i * PIXEL_SIZE))
+            pygame.gfxdraw.hline(win, 0, win_wt, i * PIXEL_SIZE, pygame.Color(40, 40, 40))
 
         for i in range(self.x):
-            pygame.draw.line(win, pygame.Color(40, 40, 40), (i * PIXEL_SIZE, 0), (i * PIXEL_SIZE, win_ht))
+            # pygame.draw.line(win, pygame.Color(40, 40, 40), (i * PIXEL_SIZE, 0), (i * PIXEL_SIZE, win_ht))
+            pygame.gfxdraw.vline(win, i * PIXEL_SIZE, 0, win_ht, pygame.Color(40, 40, 40))
 
         for i, row in enumerate(self.board):
             for j, col in enumerate(row):
                 pygame.draw.rect(win, pygame.Color(row[j]), (j * PIXEL_SIZE + 1, i * PIXEL_SIZE + 1, PIXEL_SIZE - 1, PIXEL_SIZE - 1))
+
+    def draw_color_grids(self):
+        
+        pass
 
     def color_grid(self, pos, color):
         self.board[pos[0]][pos[1]] = color
@@ -111,18 +135,32 @@ def main():
         board = Board()
         win.fill(pygame.Color(WHITE))
 
+        buttons = [
+                Button(win_wt - 100, 50, "#ff0000"),
+                Button(win_wt - 130, 50, "#0000ff"),
+                Button(win_wt - 100, 80, "#00ff00"),
+                Button(win_wt - 130, 80, "#000000"),
+                Button(win_wt - 100, 110, "#ffffff"),
+                Button(win_wt - 130, 110, "#40e0d0"),
+            ]
+
         while True:
             dt = time.time() - last_time
             dt *= 120
             last_time = time.time()
 
             board.draw_grid()
+            
+            for button in buttons:
+                button.draw()
 
             for event in pygame.event.get():
                 if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                     terminate()
                 if pygame.mouse.get_pressed()[0]:
-                    board.color_grid(cursor.get_grid_pos(pygame.mouse.get_pos()), "#000000")
+                    board.color_grid(cursor.get_grid_pos(pygame.mouse.get_pos()), cursor.color)
+                    for button in buttons:
+                        button.change_color(cursor)
 
 
             win.blit(update_fps(), (0, 0))
